f57b6d567473269d566f89c2cde29897de7811db
Precious Humphrey <phumphrey23@gmail.com>
2018-03-10 19:46:44 +0000

    automatically backed up by learn

--- lib/play.rb
+++ lib/play.rb
@@ -43,3 +43,13 @@ def play(board)
   count += 1
   end
 end
+
+def turn_count(board)
+  count = 0
+  board.each do |index|
+    if index == "X" || index == "O"
+      count += 1
+    end
+  end
+    count
+end
